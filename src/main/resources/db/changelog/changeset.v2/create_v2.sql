0c3c1cba8d9b8b1a202c6b3a725e31e15bb9dbf6
Kostetrakh <kostetrakh@gmail.com>
2021-11-06 20:28:07 +0700

    liqubase changeset.v2 fixed

--- src/main/resources/db/changelog/changeset.v2/create_v2.sql
+++ src/main/resources/db/changelog/changeset.v2/create_v2.sql
@@ -1,15 +1,15 @@
-create table grades (id  serial not null, name varchar(20), lesson_id int4, student_id int4, primary key (id))
-create table groups (group_num int4 not null, journal_id int4, teacher_id int4, primary key (group_num))
-create table groups_users (group_group_num int4 not null, users_id int4 not null, primary key (group_group_num, users_id))
-create table journals (id  serial not null, group_group_num int4, primary key (id))
-create table journals_lessons (journal_id int4 not null, lessons_id int4 not null, primary key (journal_id, lessons_id))
-create table lessons (id  serial not null, lesson_date timestamp, name varchar(255), journal_id int4, primary key (id))
-create table lessons_grades (lesson_id int4 not null, grades_id int4 not null, primary key (lesson_id, grades_id))
-create table lessons_tasks (lesson_id int4 not null, tasks_id int4 not null, primary key (lesson_id, tasks_id))
-create table roles (id  serial not null, name varchar(20), primary key (id))
-create table tasks (id  serial not null, description varchar(255), lesson_id int4, primary key (id))
-create table tokens (id  serial not null, token_status boolean, token_creation_date timestamp, token_expiry_date timestamp, token varchar(500) not null, token_user_id int4, primary key (id))
-create table user_roles (user_id int4 not null, role_id int4 not null, primary key (user_id, role_id))
-create table users (id  serial not null, user_creation_date timestamp, email varchar(255), last_visited_date timestamp, password varchar(255), username varchar(255), group_id int4, primary key (id))
-create table users_grades (user_id int4 not null, grades_id int4 not null, primary key (user_id, grades_id))
-create table users_groups (user_id int4 not null, groups_group_num int4 not null, primary key (user_id, groups_group_num))
+create table grades (id  serial not null, name varchar(20), lesson_id int4, student_id int4, primary key (id));
+create table groups (group_num int4 not null, journal_id int4, teacher_id int4, primary key (group_num));
+create table groups_users (group_group_num int4 not null, users_id int4 not null, primary key (group_group_num, users_id));
+create table journals (id  serial not null, group_group_num int4, primary key (id));
+create table journals_lessons (journal_id int4 not null, lessons_id int4 not null, primary key (journal_id, lessons_id));
+create table lessons (id  serial not null, lesson_date timestamp, name varchar(255), journal_id int4, primary key (id));
+create table lessons_grades (lesson_id int4 not null, grades_id int4 not null, primary key (lesson_id, grades_id));
+create table lessons_tasks (lesson_id int4 not null, tasks_id int4 not null, primary key (lesson_id, tasks_id));
+create table roles (id  serial not null, name varchar(20), primary key (id));
+create table tasks (id  serial not null, description varchar(255), lesson_id int4, primary key (id));
+create table tokens (id  serial not null, token_status boolean, token_creation_date timestamp, token_expiry_date timestamp, token varchar(500) not null, token_user_id int4, primary key (id));
+create table user_roles (user_id int4 not null, role_id int4 not null, primary key (user_id, role_id));
+create table users (id  serial not null, user_creation_date timestamp, email varchar(255), last_visited_date timestamp, password varchar(255), username varchar(255), group_id int4, primary key (id));
+create table users_grades (user_id int4 not null, grades_id int4 not null, primary key (user_id, grades_id));
+create table users_groups (user_id int4 not null, groups_group_num int4 not null, primary key (user_id, groups_group_num));
